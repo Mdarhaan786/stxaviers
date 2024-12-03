2c241d01abf03651aeda1952dbc9ba27dca0fef1
Mdarhaan786 <shaheenbegum868@gmail.com>
2024-12-03 20:48:06 +0530

    fixing contact us

--- stxaviers/app.py
+++ stxaviers/app.py
@@ -160,7 +160,9 @@ def contact_us(data):
     # Display the Contact Us content dynamically from the data
     if 'contact_us' in data and data['contact_us']:
         st.write(data['contact_us'])  # Show the updated content
+        
     else:
+        st.write("    ")
 
         # Contact Form
     st.subheader("💬 Have Questions? Let’s Connect!")
